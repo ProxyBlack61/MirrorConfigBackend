587e2c8ff6f2b1cb14a498661bb426e33c3feea1
Kilian <kiliansc5@gmail.com>
2022-10-18 19:30:26 +0200

    Added fist mock Endpoint

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3000
+app.use(cors({
+    origin: 'http://127.0.0.1:5173'
+}));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/clockSettings', (req, res) => {
+    res.json({
+        clockSettings: {
+            showPeriod: true,
+            periodUpper: true,
+            timeFormat: true,
+            showSecond: false,
+            showWeek: false,
+            modernLook: false,
+            showSuntimes: true,
+            showDate: true,
+            showMoonTimes: true,
+        }
+    })
+});
+
+app.post('/clockSettings', (req, res) => {
+
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port http://localhost:${port}/clockSettings`)
+});
