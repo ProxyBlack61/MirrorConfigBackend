24adc10766528af0bca3c598025ae076293897f8
Kilian <kiliansc5@gmail.com>
2022-11-01 15:36:42 +0100

    Reorganized clock.controllers.js

--- Controllers/clock.controllers.js
+++ Controllers/clock.controllers.js
@@ -3,30 +3,24 @@ const fileUtils = require("../Controllers/file.utils");
 
 const getClockSettings = (req, res) => {
   console.log("GET CLOCK SETTINGS");
-  const settings = {};
-  const clockModule = MasterData.findModule("clock", MasterData.savedConfig);
 
-  settings.clockSettings = clockModule.config;
+  const settings = {};
+  const clockModule = MasterData.findModule("clock", MasterData.activeConfig);
+
+  settings.moduleEnabled = true;
+  settings.module = clockModule;
+
   res.json(settings);
 };
+
 const postClockSettings = (req, res) => {
   console.log("POST CLOCK SETTINGS");
-  let clockModule = MasterData.findModule("clock", MasterData.savedConfig);
-  clockModule.config = req.body.settings;
-  clockModule = MasterData.findModule("clock", MasterData.activeConfig);
-  if (req.body.enabled) {
-    // TODO: Build a function to add a module to the active config if it doens't exist yet
-    clockModule.config = req.body.settings;
-  } else {
-    // TODO: Build a function to remove a module from the active config
-    console.log("CLOCK DISABLED");
-    clockModule = {};
-  }
-  console.log(MasterData.savedConfig.modules);
-  console.log("--------------------");
-  console.log(MasterData.activeConfig.modules);
 
-  fileUtils.saveFile(MasterData.defaultPath, MasterData.activeConfig);
+  // Retrieve module from savedConfig & and always save settings to savedConfig
+  MasterData.removeModule("clock", MasterData.activeConfig);
+  MasterData.activeConfig.modules.push(req.body.module);
+
+  fileUtils.saveFile();
   res.end();
 };
 
